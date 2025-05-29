16f8b28224e23bf3c0bf629681695b91a6651511
Alishka <alishka.ahmadovv@gmail.com>
2025-05-29 05:44:58 +0400

    Fixed image issue

--- components/PersonalSummary.vue
+++ components/PersonalSummary.vue
@@ -2,7 +2,7 @@
     <div class="font-poppins mb-20 xl:mb-40 px-8 xl:px-30 scroll-mt-24" id="home">
         <client-only>
             <div class="flex flex-col justify-center items-center pt-20 xl:pt-40 bg-cover bg-center rounded-full" :class="isDark ? `bg-[url('/images/background.png')]` : `bg-[url('/images/background-light.png')]`">
-                <NuxtImg class="w-50 h-50 rounded-full object-cover" src="/images/profile.png" alt="Me" />
+                <img loading="lazy" class="w-50 h-50 rounded-full object-cover" src="/images/profile.png" alt="Me" />
                 <h1 class="font-bold text-2xl xl:text-5xl text-[#484E53] dark:text-white py-4">Ali Ahmadov</h1>
                 <HighlightText label="Web Developer" fontSize="text-lg xl:text-2xl" />
                 <p class="text-center text-[#484E53] dark:text-[#E1E1E1] text-base xl:text-lg max-w-[800px] pb-4">
