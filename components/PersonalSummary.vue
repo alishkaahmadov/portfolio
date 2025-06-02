44a1b38730db176c16107a28a61e929c986fcbe0
Alishka <alishka.ahmadovv@gmail.com>
2025-06-02 14:29:20 +0400

    Optimization for LCP

--- components/PersonalSummary.vue
+++ components/PersonalSummary.vue
@@ -5,38 +5,48 @@
     ref="hero"
   >
     <div
-      class="flex flex-col justify-center items-center pt-20 xl:pt-40 bg-cover bg-center rounded-full"
-      :class="
-        isDark
-          ? `bg-[url('/images/background.png')]`
-          : `bg-[url('/images/background-light.png')]`
-      "
+      class="relative flex flex-col justify-center items-center pt-20 xl:pt-40 rounded-full overflow-hidden"
     >
       <NuxtImg
-        class="w-50 h-50 rounded-full object-cover"
-        src="/images/profile.png"
-        alt="Me"
+        class="absolute inset-0 w-full h-full object-cover"
+        :src="
+          isDark ? '/images/background.png' : '/images/background-light.png'
+        "
+        alt="Background"
         format="webp"
+        preload
+        priority
       />
-      <h1
-        class="font-bold text-2xl xl:text-5xl text-[#484E53] dark:text-white py-4"
-      >
-        Ali Ahmadov
-      </h1>
-      <HighlightText label="Web Developer" fontSize="text-lg xl:text-2xl" />
-      <p
-        class="text-center text-[#484E53] dark:text-[#E1E1E1] text-base xl:text-lg max-w-[800px] pb-4"
-      >
-        As a dedicated full stack web developer with over 5 years of experience,
-        I specialize in crafting scalable, user-focused web solutions from
-        backend architecture to dynamic frontend interfaces.
-      </p>
-      <BaseButton
-        label="Contact Me"
-        href="#contact"
-        textColor="text-[#484E53]"
-        hoverColor="hover:text-[#4FC3F7]"
-      />
+
+      <div class="relative z-10 flex flex-col justify-center items-center">
+        <NuxtImg
+          class="w-50 h-50 rounded-full object-cover"
+          src="/images/profile.png"
+          alt="Me"
+          format="webp"
+          preload
+          priority
+        />
+        <h1
+          class="font-bold text-2xl xl:text-5xl text-[#484E53] dark:text-white py-4"
+        >
+          Ali Ahmadov
+        </h1>
+        <HighlightText label="Web Developer" fontSize="text-lg xl:text-2xl" />
+        <p
+          class="text-center text-[#484E53] dark:text-[#E1E1E1] text-base xl:text-lg max-w-[800px] pb-4"
+        >
+          As a dedicated full stack web developer with over 5 years of
+          experience, I specialize in crafting scalable, user-focused web
+          solutions from backend architecture to dynamic frontend interfaces.
+        </p>
+        <BaseButton
+          label="Contact Me"
+          href="#contact"
+          textColor="text-[#484E53]"
+          hoverColor="hover:text-[#4FC3F7]"
+        />
+      </div>
     </div>
   </div>
 </template>
